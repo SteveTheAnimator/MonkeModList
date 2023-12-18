a6e3207ed8a864023609e85ac0149d4908ff0c70
MrBanana <logande4610@gmail.com>
2023-12-18 11:05:14 -0600

    Add project files.

---
+++ MonkeModList/ModInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace MonkeModList
+{
+    internal class ModInfo
+    {
+        public const string GUID = "com.mrbanana.gorillatag.monkemodlist";
+        public const string Name = "MonkeModList";
+        public const string Version = "1.0.0";
+    }
+}
---
+++ MonkeModList/MonkeModList.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using ComputerPlusPlus;
+using GorillaNetworking;
+using System;
+using UnityEngine;
+using Utilla;
+
+namespace MonkeModList {
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla")]
+    [BepInDependency("com.kylethescientist.gorillatag.computerplusplus")]
+    [BepInPlugin(ModInfo.GUID, ModInfo.Name, ModInfo.Version)]
+    public class MonkeModList : BaseUnityPlugin {
+        void Start(){ Utilla.Events.GameInitialized += OnGameInitialized; }
+
+        void OnEnable() {
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+
+        void OnDisable() {
+            HarmonyPatches.RemoveHarmonyPatches();
+            enabled = true;
+        }
+
+        void OnGameInitialized(object sender, EventArgs e) {
+            
+        }
+    }
+
+    internal class ScreenData : IScreen {
+        public string Title => "MOD LIST";
+
+        public string Description => "WELCOME TO THE MONKE MOD LIST! \n <color=yellow>MADE BY MRBANANA</color>";
+
+        public string GetContent() {
+
+        }
+
+        public void OnKeyPressed(GorillaKeyboardButton button) {
+
+        }
+
+        public void Start() { }
+    }
+}
